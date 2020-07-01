a3a9235dea17e98dfef36da9870a309f97ea1558
svlachou <svlachou@epu.ntua.gr>
2020-07-02 00:42:07 +0300

    updated helpers.py

--- MCDSS/helpers.py
+++ MCDSS/helpers.py
@@ -1,7 +1,10 @@
+import os
 import numpy as np
 import pandas as pd
 import math
-import json
+from flask import Flask, jsonify, send_from_directory
+from settings import ALLOWED_EXTENSIONS, UPLOAD_FOLDER
+from werkzeug.utils import secure_filename
 
 def normalize_weights(weights):
     """ normalizes criteria's weigths """
@@ -20,8 +23,8 @@ def result_in_json(sorted_alternatives, sorted_scores):
     """ create listo of json objects for alternatives, scores and rankings """
     result = []
     for i in range(len(sorted_alternatives)):
-        result.append(json.dumps({"Alternative": sorted_alternatives[i], "Score": sorted_scores[i], "Ranking": i+1}))
-    return result
+        result.append({"Alternative": sorted_alternatives[i], "Score": sorted_scores[i], "Ranking": i+1})
+    return jsonify(result)
 
 def negate_columns(decision_matrix, optimization_type):
     """ negate columns of decision matrix in case optimization type is 1 (minimize) """
@@ -30,3 +33,16 @@ def negate_columns(decision_matrix, optimization_type):
             for i in range(len(decision_matrix)):
                 decision_matrix[i][j] = decision_matrix[i][j]*(-1.0)
     return decision_matrix
+
+def allowed_file(filename):
+    """ allowed formats to be uploaded for mcdss """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def save_file(file, path):
+    """ check file type and return path"""
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(path, filename))
+        return os.path.join(path, filename)
+    else:
+        return ""
